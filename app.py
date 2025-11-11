0f53f2031ac79a689f88fbac6115abdc918f5deb
Nombre Usuario <damibravoag@gmail.com>
2025-11-11 19:46:55 -0300

    uwu

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from auxiliares import nombre_aplicacion, numero_version
-from datos.conexion import test_connection
-from iu import menu_gestion_autores, menu_gestion_clientes, menu_gestion_libros, menu_gestion_suscripcion, menu_principal
-from negocio import crear_libro, crear_tipo_suscripcion, modificar_libro, modificar_tipo_suscripcion, obtener_listado_libros, obtener_listado_tipos_suscripcion
+from iu import menu_principal, menu_gestion_libros, menu_gestion_suscripcion, menu_gestion_clientes, menu_gestion_autores
+from negocio import (
+    obtener_listado_libros, crear_libro, modificar_libro,
+    obtener_listado_tipos_suscripcion, crear_tipo_suscripcion, modificar_tipo_suscripcion,
+)
 
 seleccionar_opcion = 'Seleccione su opcion [0-4]: '
 opcion_incorrecta = 'Opcion Incorrecta, ingrese nuevamente...'
@@ -9,19 +11,15 @@ volver_menu_principal = 'Volviendo al menu principal...'
 
 print(f'{nombre_aplicacion} {numero_version}')
 
-if not test_connection():
-    print("Advertencia: no se pudo establecer conexión con la base de datos.")
-    print("Algunas funciones que requieren persistencia fallarán. Revisa DATABASE_URL y el servidor MySQL.")
-
 try:
     while True:
         print()
         menu_principal()
-        opcion = input('Seleccione su opcion [0-4]: ')
+        opcion = input(seleccionar_opcion).strip()
         if opcion == '1':
             while True:
                 menu_gestion_libros()
-                opcion_libro = input(seleccionar_opcion)
+                opcion_libro = input(seleccionar_opcion).strip()
                 if opcion_libro == '1':
                     crear_libro()
                 elif opcion_libro == '2':
@@ -38,7 +36,7 @@ try:
         elif opcion == '2':
             while True:
                 menu_gestion_suscripcion()
-                opcion_suscripcion = input(seleccionar_opcion)
+                opcion_suscripcion = input(seleccionar_opcion).strip()
                 if opcion_suscripcion == '1':
                     crear_tipo_suscripcion()
                 elif opcion_suscripcion == '2':
@@ -55,7 +53,7 @@ try:
         elif opcion == '3':
             while True:
                 menu_gestion_clientes()
-                opcion_cliente = input(seleccionar_opcion)
+                opcion_cliente = input(seleccionar_opcion).strip()
                 if opcion_cliente == '1':
                     print('Registrar nuevo Cliente (Pendiente)')
                 elif opcion_cliente == '2':
@@ -72,7 +70,7 @@ try:
         elif opcion == '4':
             while True:
                 menu_gestion_autores()
-                opcion_autor = input(seleccionar_opcion)
+                opcion_autor = input(seleccionar_opcion).strip()
                 if opcion_autor == '1':
                     print('Registrar nuevo Autor (Pendiente)')
                 elif opcion_autor == '2':
